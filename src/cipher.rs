caa3cbd925b67be23fba1da14e44a8be72a9f5d2
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2020-03-31 20:17:30 +0200

    Simplified test code

--- src/cipher.rs
+++ src/cipher.rs
@@ -50,18 +50,12 @@ impl TuyaCipher {
 }
 
 #[test]
-fn encrypt_message_as_a_buffer() {
+fn encrypt_message() {
     let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
 
-    let data = json::parse(
-        r#"{"devId": "002004265ccf7fb1b659",
-            "dps": {"1": false, "2": 0},
-            "t": 1529442366,
-            "s": 8}"#,
-    )
-    .map(|d| json::stringify(d))
-    .map(|d| d.as_bytes().to_owned())
-    .unwrap();
+    let data = r#"{"devId":"002004265ccf7fb1b659","dps":{"1":false,"2":0},"t":1529442366,"s":8}"#
+        .as_bytes()
+        .to_owned();
 
     let base64 = false;
     let result = cipher.encrypt(&data, base64).unwrap();
@@ -71,18 +65,23 @@ fn encrypt_message_as_a_buffer() {
 }
 
 #[test]
+fn decrypt_message_without_header_and_base64_encoding() {
+    let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
+    let message = b"3.133ed3d4a21effe90zrA8OK3r3JMiUXpXDWauNppY4Am2c8rZ6sb4Yf15MjM8n5ByDx+QWeCZtcrPqddxLrhm906bSKbQAFtT1uCp+zP5AxlqJf5d0Pp2OxyXyjg=";
+    let expected =
+        r#"{"devId":"002004265ccf7fb1b659","dps":{"1":false,"2":0},"t":1529442366,"s":8}"#
+            .as_bytes()
+            .to_owned();
+}
+
+#[test]
 fn decrypt_message_with_header_and_base_64_encoding() {
     let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
     let message = b"3.133ed3d4a21effe90zrA8OK3r3JMiUXpXDWauNppY4Am2c8rZ6sb4Yf15MjM8n5ByDx+QWeCZtcrPqddxLrhm906bSKbQAFtT1uCp+zP5AxlqJf5d0Pp2OxyXyjg=";
-    let expected = json::parse(
-        r#"{"devId": "002004265ccf7fb1b659",
-            "dps": {"1": false, "2": 0},
-            "t": 1529442366,
-            "s": 8}"#,
-    )
-    .map(|e| json::stringify(e))
-    .map(|e| e.as_bytes().to_owned())
-    .unwrap();
+    let expected =
+        r#"{"devId":"002004265ccf7fb1b659","dps":{"1":false,"2":0},"t":1529442366,"s":8}"#
+            .as_bytes()
+            .to_owned();
 
     let result = cipher.decrypt(message).unwrap();
     assert_eq!(expected, result);
