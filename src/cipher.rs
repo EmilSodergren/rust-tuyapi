caa72f6e8606f45252f65d73e89619d58e10c7f8
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2020-04-01 20:51:43 +0200

    Improved decrypt to handle version 3.3

--- src/cipher.rs
+++ src/cipher.rs
@@ -11,6 +11,10 @@ pub(crate) struct TuyaCipher {
     cipher: Cipher,
 }
 
+fn contains_header(version: &TuyaVersion, data: &[u8]) -> bool {
+    data.len() > 3 && &data[..3] == version.as_bytes()
+}
+
 impl TuyaCipher {
     pub fn create(key: String, version: TuyaVersion) -> TuyaCipher {
         TuyaCipher {
@@ -30,14 +34,11 @@ impl TuyaCipher {
         }
     }
 
-    fn contains_header(&self, data: &[u8]) -> bool {
-        data.len() > 3 && &data[..3] == self.version.as_bytes()
-    }
-
     pub fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>> {
         // Different header size in version 3.1 and 3.3
         // 3.1 is base64 encoded, 3.3 is not
-        let data = if self.contains_header(&data) {
+        let data = if contains_header(&self.version, &data) {
+            // Handle the header
             match self.version {
                 TuyaVersion::ThreeOne => {
                     let (_, data) = data.split_at(19);
@@ -46,11 +47,16 @@ impl TuyaCipher {
                 TuyaVersion::ThreeThree => data.split_at(15).1.to_vec(),
             }
         } else {
-            data.to_vec()
+            // Handle No header
+            match self.version {
+                TuyaVersion::ThreeOne => {
+                    base64::decode(data).map_err(|e| ErrorKind::Base64DecodeError(e))?
+                }
+                TuyaVersion::ThreeThree => data.to_vec(),
+            }
         };
-        // let udpkey_hash = md5::compute(UDP_KEY);
         let res = decrypt(self.cipher, &self.key.as_bytes(), None, &data)
-            // .or(decrypt(self.cipher, udpkey_hash.as_bytes(), None, &data))
+            .or(decrypt(self.cipher, &md5::compute(UDP_KEY).0, None, &data))
             .map_err(|e| ErrorKind::DecryptionError(e))?;
 
         Ok(res.to_vec())
@@ -60,21 +66,20 @@ impl TuyaCipher {
 #[test]
 fn test_contains_header_with_correct_header() {
     let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
-    assert_eq!(cipher.contains_header(b"3.133ed3d4a2..."), true)
+    assert_eq!(contains_header(&cipher.version, b"3.133ed3d4a2..."), true)
 }
 
 #[test]
 fn test_contains_header_with_wrong_header() {
     let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
-    assert_eq!(cipher.contains_header(b"3.333ed3d4a2..."), false)
+    assert_eq!(contains_header(&cipher.version, b"3.333ed3d4a2..."), false)
 }
 
 #[test]
 fn test_contains_header_with_no_header() {
     let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
-    assert_eq!(cipher.contains_header(b"3.333ed3d4a2..."), false)
+    assert_eq!(contains_header(&cipher.version, b"zrA8OK3r3JMi.."), false)
 }
-
 #[test]
 fn encrypt_message() {
     let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
@@ -101,6 +106,19 @@ fn decrypt_message_with_header_and_base_64_encoding() {
 }
 
 #[test]
+fn decrypt_message_without_header_and_without_base64_encoding() {
+    let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeThree);
+    let message = hex::decode("CEB03C38ADEBDC9322517A570D66AE369A58E009B673CAD9EAC6F861FD7932333C9F90720F1F9059E099B5CACFA9D7712EB866F74E9B48A6D0005B53D6E0A9FB33F903196A25FE5DD0FA763B1C97CA38").unwrap();
+    let expected =
+        r#"{"devId":"002004265ccf7fb1b659","dps":{"1":false,"2":0},"t":1529442366,"s":8}"#
+            .as_bytes()
+            .to_owned();
+
+    let decrypted = cipher.decrypt(&message).unwrap();
+    assert_eq!(&expected, &decrypted);
+}
+
+#[test]
 fn decrypt_message_without_header_and_base64_encoding() {
     let cipher = TuyaCipher::create("bbe88b3f4106d354".to_string(), TuyaVersion::ThreeOne);
     let message = b"zrA8OK3r3JMiUXpXDWauNppY4Am2c8rZ6sb4Yf15MjM8n5ByDx+QWeCZtcrPqddxLrhm906bSKbQAFtT1uCp+zP5AxlqJf5d0Pp2OxyXyjg=";
@@ -119,6 +137,6 @@ fn decrypt_message_where_payload_is_not_json() {
     let message = b"3.133ed3d4a21effe90rt1hJFzMJPF3x9UhPTCiXw==";
     let expected = "gw id invalid".as_bytes().to_owned();
 
-    // let decrypted = cipher.decrypt(message).unwrap();
-    // assert_eq!(&expected, &decrypted);
+    let decrypted = cipher.decrypt(message).unwrap();
+    assert_eq!(&expected, &decrypted);
 }
