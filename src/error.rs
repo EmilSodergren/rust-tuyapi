b0addecd295026bb401bb8ef2e5d1e53fb22acb6
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2020-10-05 18:43:02 +0200

    Reduced boilerplate by introduce thiserror crate

--- src/error.rs
+++ src/error.rs
@@ -1,50 +1,38 @@
 use base64::DecodeError;
 use openssl::error::ErrorStack;
-use std::error::Error;
-use std::fmt;
 use std::io;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum ErrorKind {
+    #[error("String failed to decode as base64, error was: {0}")]
     Base64DecodeError(DecodeError),
+    #[error("Something went wrong when parsing the received buffer. It still contains data after parsing is done.")]
     BufferNotCompletelyParsedError,
+    #[error("Can not encode messages that are missing CommandType")]
     CanNotEncodeMessageWithoutCommand,
+    #[error("No CommandType was supplied in message")]
     CommandTypeMissing,
+    #[error("Error: CRC mismatch")]
     CRCError,
+    #[error("Decryption failed with: {0}")]
     DecryptionError(ErrorStack),
+    #[error("Encryption failed with: {0}")]
     EncryptionError(ErrorStack),
+    #[error("Json failed: {0}")]
     JsonError(serde_json::error::Error),
+    #[error("The key length is {0}, should be 16")]
     KeyLength(usize),
+    #[error("the tuyadevice is not created with a socket address. can not set object.")]
     MissingAddressError,
+    #[error("parsing failed with: {0:?}")]
     ParseError(nom::error::ErrorKind),
+    #[error("Data was incomplete. Error while parsing the received data")]
     ParsingIncomplete,
+    #[error("{0}")]
     SystemTimeError(std::time::SystemTimeError),
+    #[error("Could not write to TcpStream. Error: {0}")]
     TcpError(io::Error),
+    #[error("The given version {0}.{1} is not valid")]
     VersionError(String, String),
 }
-
-impl fmt::Display for ErrorKind {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use ErrorKind::*;
-        let msg = match &self {
-            Base64DecodeError(err) => format!("String failed to decode as base64, error was: {}", err),
-            BufferNotCompletelyParsedError => "Something went wrong when parsing the received buffer. It still contains data after parsing is done.".to_string(),
-            CanNotEncodeMessageWithoutCommand => "Can not encode messages that are missing CommandType".to_string(),
-            CommandTypeMissing => "No CommandType was supplied in message".to_string(),
-            CRCError => "Error: CRC mismatch".to_string(),
-            DecryptionError(err) => format!("Decryption failed with: {}", err),
-            EncryptionError(err) => format!("Encryption failed with: {}", err),
-            JsonError(err) => format!("Json failed: {}", err),
-            KeyLength(s) => format!("The key length is {}, should be 16", s),
-            MissingAddressError => "The TuyaDevice is not created with a socket address. Can not set object.".to_string(),
-            ParseError(err) => format!("Parsing failed with {}", err.description()),
-            ParsingIncomplete => "Data was incomplete. Error while parsing the received data".to_string(),
-            SystemTimeError(err) => format!("{}", err),
-            TcpError(err) => format!("Could not write to TcpStream. Error: {}", err),
-            VersionError(maj, min) => format!("The given version {}.{} is not valid", maj, min),
-        };
-        write!(f, "{}", msg)
-    }
-}
-
-impl Error for ErrorKind {}
