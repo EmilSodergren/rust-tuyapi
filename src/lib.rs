02ea0ad90dd0d7f191b71c962a7e01647962bc76
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2021-02-28 20:56:14 +0100

    Add enum for Message Payload

--- src/lib.rs
+++ src/lib.rs
@@ -14,19 +14,75 @@ use std::time::SystemTime;
 
 use crate::mesparse::Result;
 use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::fmt::Display;
+
+use crate::error::ErrorKind;
 
 pub enum TuyaType {
     Socket,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Clone, PartialEq)]
+enum Payload {
+    Struct(PayloadStruct),
+    String(String),
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 #[allow(non_snake_case)]
-pub struct Payload {
+pub struct PayloadStruct {
     devId: String,
     gwId: String,
     uid: String,
     t: u32,
     dps: HashMap<String, serde_json::Value>,
+}
+
+pub trait Scramble {
+    fn scramble(&self) -> Self;
+
+    /// Take the last 5 characters
+    fn scramble_str(text: &str) -> &str {
+        if let Some((i, _)) = text.char_indices().rev().nth(5) {
+            return &text[i..];
+        }
+        text
+    }
+}
+
+impl TryFrom<Vec<u8>> for Payload {
+    type Error = ErrorKind;
+
+    fn try_from(vec: Vec<u8>) -> Result<Self> {
+        Ok(match serde_json::from_slice(&vec)? {
+            serde_json::Value::String(s) => Payload::String(s),
+            value => Payload::Struct(serde_json::from_value(value)?),
+        })
+    }
+}
+
+impl Scramble for PayloadStruct {
+    fn scramble(&self) -> PayloadStruct {
+        PayloadStruct {
+            devId: String::from("...") + Self::scramble_str(&self.devId),
+            gwId: String::from("...") + Self::scramble_str(&self.gwId),
+            t: self.t,
+            uid: self.uid.clone(),
+            dps: self.dps.clone(),
+        }
+    }
+}
+
+impl Display for PayloadStruct {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let full_display = std::env::var("TUYA_FULL_DISPLAY").map_or_else(|_| false, |_| true);
+        if full_display {
+            write!(f, "{}", serde_json::to_string(self).unwrap())
+        } else {
+            write!(f, "{}", serde_json::to_string(&self.scramble()).unwrap())
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -55,7 +111,7 @@ pub struct GetPayload {
 // }
 //
 pub fn payload(device_id: &str, tt: TuyaType, state: &str) -> Result<String> {
-    serde_json::to_string(&Payload {
+    serde_json::to_string(&PayloadStruct {
         devId: device_id.to_string(),
         gwId: device_id.to_string(),
         uid: "".to_string(),
