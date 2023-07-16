3aed9a33fd82142e518529e8431f7c3bf03a2627
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2023-07-16 22:02:22 +0200

    First attempt to implement a udp transport feature

--- src/tuyadevice.rs
+++ src/tuyadevice.rs
@@ -10,7 +10,7 @@ use crate::mesparse::{CommandType, Message, MessageParser};
 use crate::{Payload, Result};
 use log::{debug, info};
 use std::io::prelude::*;
-use std::net::{IpAddr, Shutdown, SocketAddr, TcpStream};
+use std::net::{IpAddr, Shutdown, SocketAddr, TcpStream, UdpSocket};
 use std::time::Duration;
 
 pub enum Transport {
@@ -80,7 +80,7 @@ impl TuyaDevice {
     fn send(&self, mes: &Message, seq_id: u32) -> Result<Vec<Message>> {
         match self.transport {
             Transport::TCP(_) => self.send_with_tcp(mes, seq_id),
-            Transport::UDP(_) => todo!(),
+            Transport::UDP(_) => self.send_with_udp(mes, seq_id),
         }
     }
 
@@ -88,8 +88,10 @@ impl TuyaDevice {
         let mut tcpstream = TcpStream::connect(self.addr)?;
         tcpstream.set_nodelay(true)?;
         tcpstream.set_read_timeout(Some(Duration::new(2, 0)))?;
-        tcpstream.set_read_timeout(Some(Duration::new(2, 0)))?;
-        info!("Writing message to {} ({}):\n{}", self.addr, seq_id, &mes);
+        info!(
+            "Writing message to TCP:{} ({}):\n{}",
+            self.addr, seq_id, &mes
+        );
         let bts = tcpstream.write(self.mp.encode(mes, true)?.as_ref())?;
         info!("Wrote {} bytes ({})", bts, seq_id);
         let mut buf = [0; 256];
@@ -104,8 +106,32 @@ impl TuyaDevice {
                 hex::encode(&buf[..bts])
             );
         }
-        debug!("Shutting down connection ({})", seq_id);
+        debug!("Shutting down TCP connection ({})", seq_id);
         tcpstream.shutdown(Shutdown::Both)?;
+        self.mp.parse(&buf[..bts])
+    }
+
+    fn send_with_udp(&self, mes: &Message, seq_id: u32) -> Result<Vec<Message>> {
+        let udpsocket = UdpSocket::bind(self.addr)?;
+        udpsocket.set_read_timeout(Some(Duration::new(2, 0)))?;
+        info!(
+            "Writing message to UDP:{} ({}):\n{}",
+            self.addr, seq_id, &mes
+        );
+        let bts = udpsocket.send(self.mp.encode(mes, true)?.as_ref())?;
+        info!("Wrote {} bytes ({})", bts, seq_id);
+        let mut buf = [0; 256];
+        let bts = udpsocket.recv(&mut buf)?;
+        info!("Received {} bytes ({})", bts, seq_id);
+        if bts == 0 {
+            return Err(ErrorKind::BadTcpRead);
+        } else {
+            debug!(
+                "Received response ({}):\n{}",
+                seq_id,
+                hex::encode(&buf[..bts])
+            );
+        }
         self.mp.parse(&buf[..bts])
     }
 }
